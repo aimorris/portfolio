a7ed3045f884b5cefbd440dab34b21b76d475717
Adam Morris <amorris2003@gmail.com>
2020-05-06 12:46:58 +1000

    remove logo

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,16 +1,6 @@
 <template>
   <header class="header">
     <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navigation-bar">
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
       <div id="navigation-bar" class="navbar-menu">
         <div class="navbar-start">
           <a href="/" class="navbar-item">Home</a>
