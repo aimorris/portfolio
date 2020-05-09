194173b9502adaea03a8cfdde83b1c3e67a26f4b
Adam Morris <amorris2003@gmail.com>
2020-05-09 14:36:45 +1000

    about page

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <p>
+      I am a front-end web developer from Brisbane specialising in positive user experience and responsive design. <br> <br>
+      I have developed applications using frameworks such as
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://vuejs.org/">Vue.js</a>,
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://nuxtjs.org/">Nuxt.js</a>,
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://p5js.org/">p5.js</a>, and
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://bulma.io/">Bulma</a>.
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.framework-link {
+  text-decoration: underline;
+}
+</style>
