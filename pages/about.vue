a9b8cd4db279dbc7b09f9a007e2a2eb94cac96d5
Adam Morris <amorris2003@gmail.com>
2020-05-09 15:34:09 +1000

    projects and about

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,20 @@
   <div class="container">
     <p>
       I am a front-end web developer from Brisbane specialising in positive user experience and responsive design. <br> <br>
-      I have developed applications using frameworks such as
+      I have developed applications using frameworks/technologies such as
       <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://vuejs.org/">Vue.js</a>,
       <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://nuxtjs.org/">Nuxt.js</a>,
-      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://p5js.org/">p5.js</a>, and
-      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://bulma.io/">Bulma</a>.
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://p5js.org/">p5.js</a>,
+        <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://processing.org//">Processing</a>,
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://bulma.io/">Bulma</a>,
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://jquery.com/">jQuery</a>,
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://sass-lang.com/">SASS</a>, and
+      <a target="_blank" rel="noopener noreferrer" class="framework-link" href="https://stylus-lang.com/">Stylus</a>.
+
+      <br>
+      <br>
+
+      Languages I am comfortable with using in development include HTML, CSS, JavaScript, and Java.
     </p>
   </div>
 </template>
---
+++ pages/projects.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="container">
+    <carousel
+      :perPage="1"
+      :paginationEnabled="false"
+      :navigationEnabled="true"
+      :navigationNextLabel="'>'"
+      :navigationPrevLabel="'<'"
+    >
+      <slide>
+        <img src="~/assets/exercism.svg" alt="Exercism logo">
+        <div class="text">
+          <h1>Exercism V3</h1>
+          <br>
+          <p>
+            I currently maintain the PureScript, CoffeeScript, JavaScript, and Haskell V3 tracks. I am leading the development of the PureScript track.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/exercism/v3" class="button--custom">GitHub</a>
+        </div>
+      </slide>
+
+      <slide>
+        <img src="~/assets/exercism.svg" alt="Exercism logo">
+        <div class="text">
+          <h1>Exercism V2 Solutions</h1>
+          <br>
+          <p>
+            My solutions to Exercism V2 problems in various programming languages.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/aimorris/exercism-v2-solutions" class="button--custom">GitHub</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://exercism.io" class="button--custom">Site</a>
+        </div>
+      </slide>
+
+      <slide>
+        <img src="~/assets/flightscope.png" alt="Flightscope Aviation logo" style="width: 100%">
+        <div class="text">
+          <h1>Flightscope Aviation Website</h1>
+          <br>
+          <p>
+            I developed the Flightscope Aviation website from scratch using HTML, CSS, JavaScript, and some minimal JQuery.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://gitlab.com/flightscope/flightscopeaviation.com.au" class="button--custom">GitLab</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://flightscope.gitlab.io/flightscopeaviation.com.au/" class="button--custom">Site</a>
+        </div>
+      </slide>
+
+      <slide>
+        <img src="~/assets/lichess.svg" alt="Lichess logo">
+        <div class="text">
+          <h1>Lichess</h1>
+          <br>
+          <p>
+            I have contributed to the Lichess website by providing new user interface features including bug fixes.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/ornicar/lila" class="button--custom">GitHub</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://lichess.org" class="button--custom">Site</a>
+        </div>
+      </slide>
+
+      <slide>
+        <img src="~/assets/upenn.png" alt="University of Pennsylvania logo">
+        <div class="text">
+          <h1>CIS194 Solutions</h1>
+          <br>
+          <p>
+            My solutions to the University of Pennsylvania's CIS194 Spring 2013 course.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/aimorris/cis194" class="button--custom">GitHub</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.cis.upenn.edu/~cis194/spring13/" class="button--custom">Site</a>
+        </div>
+      </slide>
+
+      <slide>
+        <img src="~/assets/game-of-life.gif" alt="Game of Life logo">
+        <div class="text">
+          <h1>TroTae's implementation of Conway's Game of Life</h1>
+          <br>
+          <p>
+            I have implemented new patterns for TroyTae's implementation of Conway's Game of Life for the web, written in TypeScript.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/troytae/game-of-life" class="button--custom">GitHub</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://troytae.github.io/game-of-life/" class="button--custom">Site</a>
+        </div>
+      </slide>
+
+      <slide>
+        <img src="~/assets/game-of-life.gif" alt="Game of Life logo">
+        <div class="text">
+          <h1>TroTae's implementation of Conway's Game of Life</h1>
+          <br>
+          <p>
+            I have implemented new patterns for TroyTae's implementation of Conway's Game of Life for the web, written in TypeScript.
+          </p>
+          <br>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/troytae/game-of-life" class="button--custom">GitHub</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://troytae.github.io/game-of-life/" class="button--custom">Site</a>
+        </div>
+      </slide>
+
+    </carousel>
+  </div>
+</template>
+
+<style lang="scss">
+  .VueCarousel {
+    width: 100vw;
+    .VueCarousel-navigation-button {
+      font-size: 3vw;
+      color: #191D32;
+      transition: color 0.3s;
+      outline: none;
+    }
+
+    .VueCarousel-slide {
+      display: grid;
+      grid-template-columns: 1fr 6fr;
+      width: 10vw;
+      padding: 0 5vw;
+      align-items: center;
+      text-align: center;
+
+      img {
+        height: auto;
+        width: 80%;
+      }
+
+      p {
+        font-size: 1em;
+      }
+
+      h1 {
+        font-weight: bold;
+      }
+
+      .button--custom {
+        text-decoration: none;
+        padding: 1vw 3vw;
+        margin: 0 1.5vw;
+        display: inline-block;
+        border-radius: 4px;
+        border: 1px solid;
+        border-color: #666;
+        color: #666;
+
+        transition: all 0.2s;
+      }
+
+      .button--custom:hover {
+        color: #fff;
+        background-color: #666;
+      }
+    }
+  }
+
+  .author {
+    font-weight: bold;
+  }
+</style>
